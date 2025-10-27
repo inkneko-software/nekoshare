281fa10e70caf866a7c07006710ec08b53e67ca1
leaf-lxh <meleaf@qq.com>
2025-10-27 22:06:28 +0800

    refactor: 重构为异步执行计算任务

--- research/group_breakout/breakout.py
+++ research/group_breakout/breakout.py
@@ -1,4 +1,6 @@
 from math import sqrt
+import queue
+from typing import Optional
 from entity.StockDayPrice import StockDayPrice
 import akshare as ak
 from datetime import date
@@ -47,7 +49,7 @@ class Rectangle(BaseModel):
 
 def is_recent_flat_consolidation(
     candlesticks: list[Candlestick],
-) -> tuple[Rectangle, Rectangle]:
+) -> tuple[Rectangle, Rectangle, Rectangle]:
     """
     判断最近是否横盘调整
     思路是，选取当前区间的最高点，然后选取当前区间的10%高点，
@@ -87,34 +89,50 @@ def is_recent_flat_consolidation(
             for price in prices[:selected_num]:
                 sigma_l += (price - mu_l) ** 2
             sigma_l = sqrt(sigma_l / selected_num)
-
+            # 如果6日内涨幅超过25%，则跳过
             if ((mu_h - mu_l) / mu_l) > 0.25:
                 continue
             possible_regions.append((i, j, mu_h, mu_l, sigma_h, sigma_l))
 
+    if len(possible_regions) == 0:
+        return (None, None, None)
     #取最近且波动最小的箱体
     possible_regions.sort(key=lambda r: (r[1]) ,reverse=True)
     tmp = possible_regions[: round(len(possible_regions) / 3)]
     tmp.sort(key=lambda r: (r[4] + r[5]))
-    nearest_box = tmp[0]
+    recent_box = tmp[0] if len(tmp) > 0 else None
     #取波动最小且长度最大的箱体
     possible_regions.sort(key=lambda r: (r[4] + r[5]))
     tmp = possible_regions[: round(len(possible_regions) / 3)]
     tmp.sort(key=lambda r: (r[1] - r[0]), reverse=True)
-    best_box = tmp[0]
+    best_box = tmp[0] if len(tmp) > 0 else None
+    #nearest_box为当前区间最后一天前为右边界的箱体
+    region = [r for r in possible_regions if (r[1] == len(candlesticks) - 1)]
+    region.sort(key=lambda r: (r[4] + r[5]))
+    tmp = region[: round(len(region) / 3)]
+    tmp.sort(key=lambda r: (r[1] - r[0]), reverse=True)
+    nearest_box = tmp[0] if len(tmp) > 0 else None
+
     return [
         Rectangle(
             start_date=candlesticks[nearest_box[0]].date,
             end_date=candlesticks[nearest_box[1]].date,
             high_price=nearest_box[2],
             low_price=nearest_box[3],
-        ),
+        ) if nearest_box is not None else None,
+        Rectangle(
+            start_date=candlesticks[recent_box[0]].date,
+            end_date=candlesticks[recent_box[1]].date,
+            high_price=recent_box[2],
+            low_price=recent_box[3],
+        ) if recent_box is not None else None,
         Rectangle(
             start_date=candlesticks[best_box[0]].date,
             end_date=candlesticks[best_box[1]].date,
             high_price=best_box[2],
             low_price=best_box[3],
-        )
+        ) if best_box is not None else None,
+        
         
     ]
 
@@ -182,67 +200,41 @@ def is_break_out(candlesticks: list[StockDayPrice]) -> ProbeResult:
 
     return False
 
+class BreakoutResult(BaseModel):
+    is_break_out: bool
+    new_high_days: float
 
-def main():
-    # 选取当日突破板块，并选取其成分股中突破的个股
-    start_date = "20250601"
-    end_date = date.today().strftime("%Y%m%d")
-    end_date = "20251020"
+class BreakoutStrategyExecutingResult(BaseModel):
+    type: str
+    code: str
+    name: str
+    change_pct: float
+    result: BreakoutResult
+    rectangle_nearest: Optional[Rectangle] = None
+    rectangle_recent: Optional[Rectangle] = None
+    rectangle_large: Optional[Rectangle] = None
 
-    # 获取所有行业板块
-    industries = nk.get_ths_industry_market()
-    selected_industries = []
-    results = []
-    for industry in industries:
-        d = []
-        i = 0
-        ret_day = nk.get_ths_industry_day_price(
-            code=industry.code,
-            start_date=start_date,
-            end_date=end_date,  # date.today().strftime("%Y%m%d")
-        )
-        d = [
-            Candlestick(
-                date=data.trade_date,
-                open=data.open,
-                high=data.high,
-                low=data.low,
-                close=data.close,
-                volume=data.volume,
-                change_pct=round(
-                    (data.close - data.pre_close) / data.pre_close * 100, 2
-                ),
-                pre_close=data.pre_close,
-            )
-            for data in ret_day
-        ]
-        industry.change_pct = d[-1].change_pct
-        if (
-            d[-1].change_pct > 0
-            and (result := is_break_out(d))
-            and result.new_high_days > 5
-        ):
-            industry.result = result
-            selected_industries.append(industry)
 
-    selected_industries.sort(key=lambda ind: ind.change_pct, reverse=True)
-    # 获取所有选中板块的成分股
-    for industry in selected_industries:
-        print(f"突破板块: {industry.name}, {industry.change_pct}% {industry.result}")
-        stocks = nk.get_ths_industry_stocks(industry.code)
-        for stock in stocks:
-            # 只做主板
-            if not stock.stock_code.startswith(("6", "0")):
-                continue
-            if stock.stock_code.startswith(("*", "ST", "退", "N", "C")):
-                continue
+def breakout(resultQueue: queue.Queue[BreakoutStrategyExecutingResult], start_date = "20250601", end_date = "20251027"):
+    print(start_date, end_date)
+    try:
+        # 选取当日突破板块，并选取其成分股中突破的个股
+        # end_date = date.today().strftime("%Y%m%d")
+        # 获取所有行业板块
+        industries = nk.get_ths_industry_market()
+        selected_industries = []
+        results = []
+        for industry in industries:
             d = []
             i = 0
-            ret_day = nk.get_stock_day_price(
-                code=stock.stock_code,
+            ret_day = nk.get_ths_industry_day_price(
+                code=industry.code,
                 start_date=start_date,
                 end_date=end_date,  # date.today().strftime("%Y%m%d")
             )
+            if len(ret_day) < 30:
+                continue
+            
             d = [
                 Candlestick(
                     date=data.trade_date,
@@ -251,17 +243,103 @@ def main():
                     low=data.low,
                     close=data.close,
                     volume=data.volume,
-                    change_pct=data.percent_change,
+                    change_pct=round(
+                        (data.close - data.pre_close) / data.pre_close * 100, 2
+                    ),
                     pre_close=data.pre_close,
                 )
                 for data in ret_day
             ]
-            (a, b) = is_recent_flat_consolidation(d)
-            if d[-1].close > a[2] and (d[-1].close - a[2]) / a[2] < 0.06 and (result := is_break_out(d)) and result.new_high_days > 5:
-                print(
-                    f"    突破个股: {stock.stock_code} {stock.stock_name},  {d[-1].change_pct}% {result}, {a}"
-                )
+            industry.change_pct = d[-1].change_pct
+            (a, b,c) = is_recent_flat_consolidation(d)
+            if (
+                d[-1].change_pct > 0
+                and (result := is_break_out(d))
+                and result.new_high_days > 5
+                and d[-1].close > a.high_price
+                and (float(d[-1].close) - a.high_price) / a.high_price < 0.05
+            ):
+                industry.result = result
+                industry.a = a
+                industry.b = b
+                industry.c = c
+                selected_industries.append(industry)
 
+        selected_industries.sort(key=lambda ind: ind.change_pct, reverse=True)
+        # 获取所有选中板块的成分股
+        for industry in selected_industries:
+            resultQueue.put(
+                BreakoutStrategyExecutingResult(
+                    type="industry",
+                    code=industry.code,
+                    name=industry.name,
+                    change_pct=float(industry.change_pct),
+                    result=BreakoutResult(
+                        is_break_out=industry.result.is_break_out,
+                        new_high_days=industry.result.new_high_days,
+                    ),
+                    rectangle_nearest=industry.a,
+                    rectangle_recent=industry.b,
+                    rectangle_large=industry.c,
+                ).model_dump_json()
+                # f"突破板块: {industry.name}, {industry.change_pct}% {industry.result}"
+            )  # 回发给客户端
+            stocks = nk.get_ths_industry_stocks(industry.code)
+            for stock in stocks:
+                # 只做主板
+                if not stock.stock_code.startswith(("6", "0")) or stock.stock_code.startswith("688"):
+                    continue
+                if stock.stock_name.startswith(("*", "ST", "退", "N", "C")):
+                    continue
+                d = []
+                i = 0
+                ret_day = nk.get_stock_day_price(
+                    code=stock.stock_code,
+                    start_date=start_date,
+                    end_date=end_date,  # date.today().strftime("%Y%m%d")
+                )
+                if len(ret_day) < 30:
+                    continue
+
+                d = [
+                    Candlestick(
+                        date=data.trade_date,
+                        open=data.open,
+                        high=data.high,
+                        low=data.low,
+                        close=data.close,
+                        volume=data.volume,
+                        change_pct=data.percent_change,
+                        pre_close=data.pre_close,
+                    )
+                    for data in ret_day
+                ]
+                print(f"板块: {industry.name}, 股票: {stock.stock_code} {stock.stock_name} {len(d)}")
+                (a, b,c) = is_recent_flat_consolidation(d)
+                if (
+                    a != None #要求6日内不能超过25
+                    and d[-1].close > a.high_price
+                    and (d[-1].close - a.high_price) / a.high_price < 0.06
+                    and (result := is_break_out(d))
+                    and result.new_high_days > 5
+                ):
+                    resultQueue.put(
+                        BreakoutStrategyExecutingResult(
+                            type="stock",
+                            code=stock.stock_code,
+                            name=stock.stock_name,
+                            change_pct=float(d[-1].change_pct),
+                            result=BreakoutResult(
+                                is_break_out=result.is_break_out,
+                                new_high_days=result.new_high_days,
+                            ),
+                            rectangle_nearest=a,
+                            rectangle_recent=b,
+                            rectangle_large=c,
+                        ).model_dump_json()
+                    )
+    finally:
+        resultQueue.put(None)
 
 
 if __name__ == "__main__":
--- research/group_breakout/http.py
+++ research/group_breakout/http.py
@@ -5,7 +5,8 @@ from pydantic import BaseModel
 import uvicorn
 import group_breakout.fetch as nk
 from group_breakout.breakout import *
-
+import queue
+import asyncio
 # class Input(BaseModel):
 #     a: float
 #     b: float
@@ -30,142 +31,41 @@ def fetch_industry_market():
     return {"code": 200, "msg": "ok"}
 
 
-class BreakoutResult(BaseModel):
-    is_break_out: bool
-    new_high_days: float
+class TimeRange(BaseModel):
+    start_date: str
+    end_date: str
 
+@app.websocket("/ws/breakout_execution")
+async def websocket_endpoint(websocket: WebSocket):
+    # 接受客户端连接
+    await websocket.accept()
+    data = await websocket.receive_json()  # 收到客户端发来的消息
+    timeRange = TimeRange(**data)
 
-class BreakoutStrategyExecutingResult(BaseModel):
-    type: str
-    code: str
-    name: str
-    change_pct: float
-    result: BreakoutResult
-    rectangle_recent: Optional[Rectangle] = None
-    rectangle_large: Optional[Rectangle] = None
+    resultQueue = queue.Queue()
+    loop = asyncio.get_running_loop()
+    loop.run_in_executor(None, breakout, resultQueue, timeRange.start_date, timeRange.end_date)
+    while True:
+        msg = await asyncio.to_thread(resultQueue.get)
+        if msg == None:
+            break
+        await websocket.send_text(msg)
+    await websocket.close()
 
-
-@app.websocket("/ws")
+@app.websocket("/ws/breakout_backtrace")
 async def websocket_endpoint(websocket: WebSocket):
     # 接受客户端连接
     await websocket.accept()
     data = await websocket.receive_text()  # 收到客户端发来的消息
-    print("客户端请求:", data)
 
-    # 选取当日突破板块，并选取其成分股中突破的个股
-    start_date = "20250601"
-    end_date = date.today().strftime("%Y%m%d")
-    end_date = "20251022"
-
-    # 获取所有行业板块
-    print("获取所有行业板块")
-    industries = nk.get_ths_industry_market()
-    selected_industries = []
-    results = []
-    for industry in industries:
-        d = []
-        i = 0
-        print(f"获取板块日线 {industry.code} {industry.name}")
-        ret_day = nk.get_ths_industry_day_price(
-            code=industry.code,
-            start_date=start_date,
-            end_date=end_date,  # date.today().strftime("%Y%m%d")
-        )
-        d = [
-            Candlestick(
-                date=data.trade_date,
-                open=data.open,
-                high=data.high,
-                low=data.low,
-                close=data.close,
-                volume=data.volume,
-                change_pct=round(
-                    (data.close - data.pre_close) / data.pre_close * 100, 2
-                ),
-                pre_close=data.pre_close,
-            )
-            for data in ret_day
-        ]
-        industry.change_pct = d[-1].change_pct
-        (a, b) = is_recent_flat_consolidation(d)
-        if (
-            d[-1].change_pct > 0
-            and (result := is_break_out(d))
-            and result.new_high_days > 5
-        ):
-            industry.result = result
-            industry.a = a
-            industry.b = b
-            selected_industries.append(industry)
-
-    selected_industries.sort(key=lambda ind: ind.change_pct, reverse=True)
-    # 获取所有选中板块的成分股
-    for industry in selected_industries:
-        await websocket.send_text(
-            BreakoutStrategyExecutingResult(
-                type="industry",
-                code=industry.code,
-                name=industry.name,
-                change_pct=float(industry.change_pct),
-                result=BreakoutResult(
-                    is_break_out=industry.result.is_break_out,
-                    new_high_days=industry.result.new_high_days,
-                ),
-                rectangle_recent=industry.a,
-                rectangle_large=industry.b,
-            ).model_dump_json()
-            # f"突破板块: {industry.name}, {industry.change_pct}% {industry.result}"
-        )  # 回发给客户端
-
-        stocks = nk.get_ths_industry_stocks(industry.code)
-        for stock in stocks:
-            # 只做主板
-            if not stock.stock_code.startswith(("6", "0")):
-                continue
-            if stock.stock_code.startswith(("*", "ST", "退", "N", "C")):
-                continue
-            d = []
-            i = 0
-            ret_day = nk.get_stock_day_price(
-                code=stock.stock_code,
-                start_date=start_date,
-                end_date=end_date,  # date.today().strftime("%Y%m%d")
-            )
-            d = [
-                Candlestick(
-                    date=data.trade_date,
-                    open=data.open,
-                    high=data.high,
-                    low=data.low,
-                    close=data.close,
-                    volume=data.volume,
-                    change_pct=data.percent_change,
-                    pre_close=data.pre_close,
-                )
-                for data in ret_day
-            ]
-            (a, b) = is_recent_flat_consolidation(d)
-            if (
-                d[-1].close > a.high_price
-                and (d[-1].close - a.high_price) / a.high_price < 0.06
-                and (result := is_break_out(d))
-                and result.new_high_days > 5
-            ):
-                await websocket.send_text(
-                    BreakoutStrategyExecutingResult(
-                        type="stock",
-                        code=stock.stock_code,
-                        name=stock.stock_name,
-                        change_pct=float(d[-1].change_pct),
-                        result=BreakoutResult(
-                            is_break_out=result.is_break_out,
-                            new_high_days=result.new_high_days,
-                        ),
-                        rectangle_recent=a,
-                        rectangle_large=b,
-                    ).model_dump_json()
-                )
-
+    resultQueue = queue.Queue()
+    loop = asyncio.get_running_loop()
+    loop.run_in_executor(None, breakout, resultQueue)
+    while True:
+        msg = await asyncio.to_thread(resultQueue.get)
+        if msg == None:
+            break
+        await websocket.send_text(msg)
     await websocket.close()
 
 
