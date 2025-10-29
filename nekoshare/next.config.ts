f5067fb784fa11bccb4292ee3aa3609dd66fa009
leaf-lxh <meleaf@qq.com>
2025-10-29 12:34:48 +0800

    fix: 由浏览器决定协议

--- nekoshare/next.config.ts
+++ nekoshare/next.config.ts
@@ -4,8 +4,9 @@ const nextConfig: NextConfig = {
   rewrites: async function () {
       return [
         {
+          //建议生产环境直接用nginx拦截直接访问pysdk
           source: '/api/pysdk/:path*',
-          destination: "http://" + process.env.NEXT_PUBLIC_PYSDK_HOST + '/api/pysdk/:path*',
+          destination: process.env.NEXT_PUBLIC_PYSDK_HOST + '/api/pysdk/:path*',
         },
       ]
   }
