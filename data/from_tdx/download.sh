2db20cdf80cfeaa1d0d247f6d3968f014e565d35
leaf-lxh <meleaf@qq.com>
2025-11-05 17:31:13 +0800

    fix: 修复路径不存在的问题

--- data/from_tdx/download.sh
+++ data/from_tdx/download.sh
@@ -1,5 +1,5 @@
 rm -r hsjday/*
-
+mkdir hsjday
 curl 'https://data.tdx.com.cn/vipdoc/hsjday.zip' \
   -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
   -H 'Accept-Language: zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7' \
