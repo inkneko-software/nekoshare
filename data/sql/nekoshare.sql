bb29d328f32509ef28dd98fad8223467be018b68
leaf-lxh <meleaf@qq.com>
2025-11-06 14:29:45 +0800

    feat: 爬虫日志表

--- data/sql/nekoshare.sql
+++ data/sql/nekoshare.sql
@@ -86,3 +86,11 @@ CREATE TABLE strategy_backtrace_task (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '任务创建时间',
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '任务最后更新时间'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE fetch_log(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT COMMENT '主键ID',
+    job_type ENUM('ths_industry_quote', 'tdx_stocks_quote') NOT NULL COMMENT '任务类型，同花顺行业实时日线/通达信实时日线',
+    job_status ENUM('success', 'failed') NOT NULL COMMENT '任务状态，成功为SUCCESS，失败为FAILED',
+    msg TEXT NOT NULL COMMENT '日志信息',
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
