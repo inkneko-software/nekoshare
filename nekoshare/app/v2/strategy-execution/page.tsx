ff7c6dbb04d822c366290ef6189a4d221b397d57
leaf-lxh <meleaf@qq.com>
2025-11-20 23:21:15 +0800

    fix: 补充未导入的本地化文件

--- nekoshare/app/v2/strategy-execution/page.tsx
+++ nekoshare/app/v2/strategy-execution/page.tsx
@@ -6,7 +6,7 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs, { Dayjs } from 'dayjs';
-
+import 'dayjs/locale/zh-cn'
 import React from 'react';
 import BackTracePriceTable from '@/components/BacktracePriceTable/BacktracePriceTable';
 import StockDayPrice from '@/lib/StockDayPrice';
