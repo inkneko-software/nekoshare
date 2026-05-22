9cb0e2af7d12bc041bcd29e2309a416a8f9cc815
leaf-lxh <meleaf@qq.com>
2026-05-22 13:54:35 +0800

    feat: 添加trade web api

--- research/web/server.py
+++ research/web/server.py
@@ -7,12 +7,14 @@ log = LoggerFactory.get_logger(__name__)
 from web.quote import router as quote_router
 from web.strategy import router as strategy_router
 from web.focus import router as focus_router
-
+from web.trade import router as trade_router
 app = FastAPI()
 
 app.include_router(quote_router)
 app.include_router(strategy_router)
 app.include_router(focus_router)
+app.include_router(trade_router)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3010)
---
+++ research/web/trade.py
@@ -0,0 +1,42 @@
+from datetime import date
+from typing import Optional
+from fastapi import FastAPI, HTTPException, WebSocket
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+import uvicorn
+import data.quote as quote
+from group_breakout.breakout import *
+from group_breakout.strategies.breakout_v1_1 import *
+from group_breakout.strategies.high_volume_breakout import *
+import queue
+import asyncio
+import group_breakout.trade_day as trade_day
+from datetime import timedelta, date, datetime
+from model import *
+from group_breakout.strategies.trend import get_rise_trend_line, get_down_trend_line
+from group_breakout.strategies import trend
+from trade.automation import tradeNextDay
+import os
+# class Input(BaseModel):
+#     a: float
+#     b: float
+
+from utils.log import LoggerFactory
+log = LoggerFactory.get_logger(__name__)
+
+base_url = "/api/pysdk"
+
+from fastapi import APIRouter
+router = APIRouter()
+
+@router.get(base_url + "/trade/getTradeResults")
+def getTradeResults(date: str = None):
+    """
+    获取某日的高量突破结果，并回测后10个交易日的表现
+
+    买入条件为 如果高开，则高开向上1%买入，如果平开或低开，需要在价格大于昨日最高价1%的时候买入
+
+    离场条件为收盘价跌破5日线，或者跌停。如果10个交易日仍未离场，则在第10个交易日收盘离场
+    """
+    results = tradeNextDay(date)
+    return results
