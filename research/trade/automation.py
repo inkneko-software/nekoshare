3983aebedd0f2ce6e662cbd98aaeecc0a96ae9d4
leaf-lxh <meleaf@qq.com>
2026-05-28 20:32:55 +0800

    fix: 修复当未来数据不足10日时，部分结果丢失的问题

--- research/trade/automation.py
+++ research/trade/automation.py
@@ -11,16 +11,23 @@ import queue
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from data.quote import get_stock_day_price
-from model.VolumeBreakoutStrategyExecutingResult import VolumeBreakoutStrategyExecutingResult
+from model.VolumeBreakoutStrategyExecutingResult import (
+    VolumeBreakoutStrategyExecutingResult,
+)
 from group_breakout import trade_day
 from strategy.high_volume_breakout import high_volume_breakout
+
 
 def get_breakout_stocks(end_date: str) -> list[VolumeBreakoutStrategyExecutingResult]:
     """
     获取某日的高量突破结果
     """
     resultQueue: queue.Queue[VolumeBreakoutStrategyExecutingResult] = queue.Queue()
-    two_years_ago = (datetime.strptime(end_date, "%Y%m%d") - relativedelta(years=1) - relativedelta(days=180)).strftime("%Y%m%d")
+    two_years_ago = (
+        datetime.strptime(end_date, "%Y%m%d")
+        - relativedelta(years=1)
+        - relativedelta(days=180)
+    ).strftime("%Y%m%d")
     high_volume_breakout(resultQueue, two_years_ago, end_date)
     # 逐个获取resultQueue中的结果,直到获取到None为止
     result = resultQueue.get()
@@ -29,6 +36,7 @@ def get_breakout_stocks(end_date: str) -> list[VolumeBreakoutStrategyExecutingRe
         ret.append(result)
         result = resultQueue.get()
     return ret
+
 
 def calculateMA(prices, n):
     """
@@ -41,6 +49,7 @@ def calculateMA(prices, n):
         return None
     return sum(prices[-n:]) / n
 
+
 @dataclass
 class TradeHistory:
     stock_code: str
@@ -48,7 +57,7 @@ class TradeHistory:
     buy_date: str
     buy_price: float
     sell_date: str
-    sell_price: float 
+    sell_price: float
     sell_reason: str
     change_pct: float
 
@@ -56,7 +65,7 @@ class TradeHistory:
 def tradeNextDay(trade_date: str) -> list[TradeHistory]:
     """
     获取指定日期的选股结果，并于下一交易日开始按照要求进行交易
-    
+
     :param date: 日期，格式为 'YYYYMMDD'
     """
     trade_date = datetime.strptime(trade_date, "%Y%m%d").date()
@@ -80,23 +89,57 @@ def tradeNextDay(trade_date: str) -> list[TradeHistory]:
         else:
             # 如果第二日平开或低开，则在价格大于昨日最高价1%的时候买入
             buy_price = prices[5].high * 1.01
-        
+
         if buy_price < prices[6].high and buy_price > prices[6].low:
             # 如果第二日的最高价都没有达到买入价，则放弃交易
             for i, price in enumerate(prices[6:]):
                 # 离场条件为收盘价跌破5日线，或者跌停。如果10个交易日仍未离场，则在第10个交易日收盘离场
-                ma5 = calculateMA([p.close for p in prices[:6+i]], 5)
+                ma5 = calculateMA([p.close for p in prices[: 6 + i]], 5)
                 if price.close < ma5:
 
-                    results.append(TradeHistory(result.code, result.name, prices[6].trade_date.strftime("%Y-%m-%d"), buy_price, price.trade_date.strftime("%Y-%m-%d"), price.close, "跌破5日线", (price.close - buy_price) / buy_price))
+                    results.append(
+                        TradeHistory(
+                            result.code,
+                            result.name,
+                            prices[6].trade_date.strftime("%Y-%m-%d"),
+                            buy_price,
+                            price.trade_date.strftime("%Y-%m-%d"),
+                            price.close,
+                            "跌破5日线",
+                            (price.close - buy_price) / buy_price,
+                        )
+                    )
                     break
                 elif price.close <= price.pre_close * 0.9:
-                    results.append(TradeHistory(result.code, result.name, prices[6].trade_date.strftime("%Y-%m-%d"), buy_price, price.trade_date.strftime("%Y-%m-%d")  , price.close, "跌停", (price.close - buy_price) / buy_price))
+                    results.append(
+                        TradeHistory(
+                            result.code,
+                            result.name,
+                            prices[6].trade_date.strftime("%Y-%m-%d"),
+                            buy_price,
+                            price.trade_date.strftime("%Y-%m-%d"),
+                            price.close,
+                            "跌停",
+                            (price.close - buy_price) / buy_price,
+                        )
+                    )
                     break
-                elif i == 9:
-                    results.append(TradeHistory(result.code, result.name, prices[6].trade_date.strftime("%Y-%m-%d"), buy_price, price.trade_date.strftime("%Y-%m-%d"), price.close, "仍持有计算收益", (price.close - buy_price) / buy_price))
+                elif i == len(prices[6:]) - 1:
+                    results.append(
+                        TradeHistory(
+                            result.code,
+                            result.name,
+                            prices[6].trade_date.strftime("%Y-%m-%d"),
+                            buy_price,
+                            price.trade_date.strftime("%Y-%m-%d"),
+                            price.close,
+                            "仍持有计算收益",
+                            (price.close - buy_price) / buy_price,
+                        )
+                    )
                     break
     return results
+
 
 if __name__ == "__main__":
     results = tradeNextDay("20260113")
